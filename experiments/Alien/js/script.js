97da51961fe0517e999c28e00757a05c2cc27fd4
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2023-09-11 18:20:19 -0400

    made an alien LMAO

--- experiments/Alien/js/script.js
+++ experiments/Alien/js/script.js
@@ -22,19 +22,37 @@ function preload() {
 function setup() {
     createCanvas(500,500)
     background(200,200,200)
+
+    stroke(255,204,0);
+
     line(175,200,1,1)
     line(325,200,500,1)
+
+    fill(0,255,0);
     ellipse(250,395,75,400)
+
+    fill(0,255,0);
     ellipse(250,200,350,175)
+    fill(0,0,0);
     circle(1,1,15)
+
+    fill(0,0,0);
     circle(500,0,25)
+
+    fill(0,0,0);
     circle(155,175,25)
+
+    fill(0,0,0);
     circle(345,175,25)
+
     line(250,270,250,200)
+    fill(0,0,0);
     ellipse(210,260,5,10)
-    ellipse(290,260,5,10)
-    line(250,112,250,0)
     
+    line(250,112,250,0)
+
+    fill(0,0,0);
+    ellipse(290,260,5,10)
 
 
     
