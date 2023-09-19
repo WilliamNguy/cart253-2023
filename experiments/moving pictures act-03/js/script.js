2984473f1bcabafd93d02d82a64693544a4f828b
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2023-09-18 21:26:44 -0400

    exercise1

--- experiments/moving pictures act-03/js/script.js
+++ experiments/moving pictures act-03/js/script.js
@@ -7,41 +7,75 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
-let backgroundShade = 0;
-
-let circle = {
-    x: 0, 
-    y: 250,
-    size: 100,
-    speed: 1,
-    fill: 255
+let bg = { 
+    r: 0,
+    g: 0,
+    b: 0
 };
+
+let circle1 = {
+    x: 0,
+    y: 390,
+    size: 250,
+    fill: 255,
+    alpha: 200
+};
+
+let rectangle = {
+    x: 1360,
+    y: 100,
+    w: 200,
+    h: 100,
+    fill: 255,
+    alpha: 200
+};
+
+let square = {
+    x: 1440,
+    y: 500,
+    size: 300,
+    fill: 255,
+    alpha: 200
+};
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-    background (backgroundShade);
+    //BACKGROUND
+    background(bg.r,bg.g,bg.b);
+    bg.r = mouseX;
+    bg.g = mouseY;
+    bg.b = 84;
 
+    //SHAPE#1
+    circle1.fill = map(mouseX,0, width, 0, 255)
+    circle1.x = circle1.x + 1;
+    circle1.x = constrain(circle1.x,0,1315);
+    fill(circle1.fill ,circle1.alpha);
+    ellipse(circle1.x, circle1.y , circle1.size);
+  
+    //SHAPE#2
     
-    circle.x = (circle.x + circle.speed);
-    circle.x = constrain(circle.x, 0, width);
+    fill(rectangle.fill ,rectangle.alpha);
+    rect(rectangle.x, mouseY,rectangle.w , mouseY);
 
+    //SHAPE#3
+    rectMode(CENTER)
+    fill(square.fill , square.alpha);
+    rect(width / 2, height / 2, mouseY);
 
-
-    //circle.size = map(mouseY, 0, height,50,500);
-
-    circle.fill = map(circle.x, 0, width,0,255);
-    fill(circle.fill);
-    ellipse(circle.x, circle.y, circle.size);
-    
+  
 
     
     
