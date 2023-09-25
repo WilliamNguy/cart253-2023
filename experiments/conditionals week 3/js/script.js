70625754f4b8f5fc048094bae2c80087904d17d4
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2023-09-25 18:04:15 -0400

    mouvement

--- experiments/conditionals week 3/js/script.js
+++ experiments/conditionals week 3/js/script.js
@@ -17,7 +17,14 @@ let bg = {
 let circle = {
     x: 250,
     y: 250,
-    size: 100
+    size: 100,
+    vx: 0,
+    vy: 0,
+    ax: 0,
+    ay: 0,
+    acceleration: 0.1,
+    maxSpeed: 10
+
 };
 
 
@@ -44,20 +51,44 @@ createCanvas (500,500);
  * Description of draw()
 */
 function draw() {
-background (bg.r, bg.b, bg.g);
+background (0);
 
 // circle.x = mouseX;
 // circle.y = mouseY;
+
+if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+}
+else {
+    circle.ax = circle.acceleration;
+}
+
+if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+}
+else {
+    circle.ay = circle.acceleration;
+}
+
+circle.vx = circle.vx + circle.ax;
+circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed)
+circle.vy = circle.vy + circle.ay;
+circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed)
+
+
+
+circle.x = circle.x + circle.vx;
+circle.y = circle.y + circle.vy;
 
 ellipse(circle.x, circle.y, circle.size);
 
 }
 
-function mouseDragged () {
-    bg.r = random(0,255);
-    bg.b = random(0,255);
-    bg.g = random(0,255);
+// function mousePressed () {
+//     bg.r = random(0,255);
+//     bg.b = random(0,255);
+//     bg.g = random(0,255);
 
-    circle.x = mouseX;
-    circle.y = mouseY;
-}
+    // circle.x = mouseX;
+    // circle.y = mouseY;
+//}
