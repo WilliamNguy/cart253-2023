f82136c1ee64c601a5f8192c273fc136d41e2375
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2023-10-30 17:01:50 -0400

    making a second paddle

--- exercises/exercise week 8/js/script.js
+++ exercises/exercise week 8/js/script.js
@@ -10,6 +10,7 @@
 
 let gravityForce = 0.0025;
 let paddle;
+let secondPaddle;
 let balls = [];
 let numBalls = 10;
 
@@ -28,6 +29,12 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 
     paddle = new Paddle(300,20);
+
+    //Creating a second paddle and putting it vertically on the right side of the screen.
+    secondPaddle = new Paddle(20,300);
+    secondPaddle.x = width - secondPaddle.width/2;
+    secondPaddle.y = height/2;
+
     
     for (let i = 0; i < numBalls; i++) {
         let x = random(0,width);
@@ -47,12 +54,17 @@ function draw() {
     paddle.move();
     paddle.display();
 
+    //mouvement and display for second paddle
+    secondPaddle.move();
+    secondPaddle.display();
+
     for (let i = 0; i < balls.length; i++) {
         let ball = balls[i];
         if (ball.active);
         ball.gravity(gravityForce);
         ball.move();
         ball.bounce(paddle);
+        ball.bounce(secondPaddle);// add collision for second paddle.
         ball.display();
     }
 }
