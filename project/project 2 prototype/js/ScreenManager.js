3f8e8af286e01083737fd0ec1e02d2aa34a03018
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2023-11-07 20:29:07 -0500

    added pdf file

--- project/project 2 prototype/js/ScreenManager.js
+++ project/project 2 prototype/js/ScreenManager.js
@@ -1,28 +1,20 @@
 class ScreenManager {
     constructor() {
         this.currentScreen = 0;
-        // this.screens = [
-        //     new Screens(color(255,0,0), this.displayScreen1),
-        //     new Screens(color(0,255,0), this.displayScreen2),
-        // ];
     }
 
-    // createScreen(r,g,b,displayFunction) {
-    //     return {r,g,b,displayFunction};
-    // }
+ 
     displayScreen1() {
         if (this.currentScreen === 0) {
             background(255,255,255);
         }
-        // fill(255,0,0);
-        // rect(width/2,height/2,100,100);
+   
     }
     displayScreen2() {
         if (this.currentScreen === 1) {
             background(0,255,0);
         }
-        // fill(0,255,0);
-        // ellipse(width/2,height/2,100,100);
+    
     }
     displayScreen3() {
         if (this.currentScreen === -1) {
@@ -75,21 +67,6 @@ class ScreenManager {
             player.y = 0;
         }
         
-        // const currentScreen = this.screens[this.currentScreen];
-        
-        // if (this.currentScreen === 0) {
-        //     this.displayScreen1();
-        // }
-        // else if (this.currentScreen === 1) {
-        //     this.displayScreen2();
-        // }
-        // else if (player.y < 0) {
-
-        // }
-        // else if (player.y > height) {
-
-        // }
-        // screenManager.screens[screenManager.currentScreen].display();
-
+ 
     }
 }
