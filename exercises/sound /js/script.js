292eddde343cb2b18fb13c6ba73e96b6f8829b89
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2023-11-12 15:08:14 -0500

    started sound

---
+++ exercises/sound /js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let barkSFX;
+
+function preload() {
+    barkSFX = loadSound(`assets/sounds/bark.wav`)
+}
+
+
+function setup() {
+    createCanvas (windowWidth, windowHeight);
+}
+
+
+
+function draw() {
+   background(0);
+}
+
+function mousePressed() {
+    barkSFX.play();
+}
