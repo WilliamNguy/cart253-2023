a45ba0e82c84649d50e5cc71811b21c7c844ddde
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2023-11-12 15:40:59 -0500

    finished first video

--- exercises/sound /js/script.js
+++ exercises/sound /js/script.js
@@ -9,14 +9,19 @@ function preload() {
 
 function setup() {
     createCanvas (windowWidth, windowHeight);
+    userStartAudio();
 }
 
 
 
 function draw() {
    background(0);
+
+   let newRate = map(mouseX,0,width,-3,3);
+   barkSFX.rate(newRate);
 }
 
 function mousePressed() {
-    barkSFX.play();
+    barkSFX.loop();
+
 }
